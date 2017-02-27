f563aa8b16d4f1f3ab37268b52155103562d83c6
mrkcntrmn <ilovepoker@gmail.com>
2017-02-26 20:24:32 -0600

    2-26-17

---
+++ csvDataTest.py
@@ -0,0 +1,22 @@
+'''
+Created on Feb 15, 2017
+
+@author: MARK
+'''
+import csv
+with open('ChildCareCenters.csv', 'rn') as file:
+    reader = csv.reader(file)
+    centers_list = list(reader)
+
+with open('ChildCareCentersEdited.csv', 'wb') as newFile:
+    writer = csv.writer(newFile, delimiter=',', quoting=csv.QUOTE_ALL)
+    writer.writerows(centers_list)
+
+with open('ChildCareCentersEdited.csv', 'rn') as testFile:
+    newReader = csv.reader(testFile)
+    test_list = list(newReader)
+
+
+
+print(test_list[0])
+print(test_list[2923])
